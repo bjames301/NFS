a4f3a1269e984f47d16ed7bd880b50c223aa3c49
Brandon James <brandonajames@gmail.com>
2011-09-26 14:29:16 -0400

    fixed server install ordering

--- manifests/server.pp
+++ manifests/server.pp
@@ -2,5 +2,5 @@ class nfs::server {
 	include nfs::server::install
 	include nfs::server::service
 
-	Class['nfs::install'] -> Class['nfs::service']
+	Class['nfs::server::install'] -> Class['nfs::server::service']
 }
